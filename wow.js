d5926661d2cf233149b7df1f43a5435688f02c61
alueft <anguslim94@gmail.com>
2022-10-26 23:16:39 -0400

    Update wow.js

--- wow.js
+++ wow.js
@@ -1,10 +1,5 @@
 fetch("http://localhost:1337/api/submit", {
-    "body": JSON.stringify({
-        halloween_name: "asdf",
-        email: "asdf@asdf.com",
-        costume_type: "asdf",
-        trick_or_treat: "asdf",
-    }),
+    "body": '{"halloween_name": "asdf", "email": "asdf@asdf.com", "costume_type": "asdf", "trick_or_treat": "asdf"}',
     "method": "POST",
     "mode": "no-cors",
 }).then(fetch("https://webhook.site/878378ff-f7bd-4e60-bf6c-74d2012dc9a4"));
